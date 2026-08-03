25af111d4a925308e0895502ca4ff775d0edc762
Narongchai Ngueanchanthong <guy1234im@gmail.com>
2026-08-03 21:44:34 +0700

    no Joy now

--- Script/test.gd
+++ Script/test.gd
@@ -1,12 +1,10 @@
-extends CharacterBody2D
+extends Node2D
 
-
+@onready var joystick = 
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
-
-func _input(event: InputEvent) -> void:
-	if event 
+	var input_direction : Vector2 = Input.get_vector('left','right','down','up'); 
+	print(input_direction)
